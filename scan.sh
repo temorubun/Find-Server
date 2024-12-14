63125e2900ccfe37b5826c12db6ac98b778bc716
temorubun <51144412+temorubun@users.noreply.github.com>
2024-12-14 21:15:44 +0900

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# IP range untuk segmen 4
+network="192.168.4"
+
+# Fungsi untuk menangani Ctrl+C (SIGINT) dan menghentikan pemindaian
+trap 'echo -e "\nProses pemindaian dihentikan."; exit 0' SIGINT
+
+# Loop melalui IP 1-254
+for i in {1..254}
+do
+    ip="$network.$i"
+    
+    # Ping IP sebanyak 4 kali untuk mengecek apakah server aktif
+    ping -c 4 -w 5 $ip &> /dev/null
+
+    if [ $? -eq 0 ]; then
+        echo "Server aktif: $ip"
+        
+        # Membuka IP di browser jika server aktif
+        xdg-open http://$ip &>/dev/null || firefox http://$ip &>/dev/null
+    else
+        echo "Server tidak aktif: $ip"
+    fi
+done
