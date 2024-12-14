7768c40271a613bbf638d5b118ffba69936ddaf2
temorubun <51144412+temorubun@users.noreply.github.com>
2024-12-14 21:28:36 +0900

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,8 +3,8 @@
 # IP range untuk segmen 4
 network="192.168.4"
 
-# Fungsi untuk menangani Ctrl+C (SIGINT) dan menghentikan pemindaian
-trap 'echo -e "\nProses pemindaian dihentikan."; exit 0' SIGINT
+# Fungsi untuk menangani Ctrl+C (SIGINT) dan menghentikan pemindaian tanpa menutup halaman
+trap 'echo -e "\nProses pemindaian dihentikan."; break' SIGINT
 
 # Loop melalui IP 1-254
 for i in {1..254}
@@ -15,13 +15,13 @@ do
     ping -c 4 -w 5 $ip &> /dev/null
 
     if [ $? -eq 0 ]; then
-        echo "Server sedang aktif: $ip"
+        echo "Server ON: $ip"
         
         # Membuka IP di browser di latar belakang agar tetap berjalan meskipun pemindaian dihentikan
-        (xdg-open http://$ip &)
-        # Jika xdg-open tidak berfungsi, gunakan firefox
-        #(firefox http://$ip &)
+        xdg-open http://$ip &>/dev/null & disown
+        # Atau, jika xdg-open tidak berfungsi, gunakan Firefox
+        # firefox http://$ip &>/dev/null & disown
     else
-        echo "Server tidak aktif: $ip"
+        echo "Server OFF: $ip"
     fi
 done
